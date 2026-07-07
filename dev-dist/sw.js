4acad00c9a292cd78a01e2b73b0b581828518bfe
ICPTuzla <aisotuzla@gmail.com>
2026-07-07 14:05:55 +0200

    chore: initialize git repository and configure plugin project files

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fvke2dmjtd"
+    "revision": "0.25k6omdv4s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
